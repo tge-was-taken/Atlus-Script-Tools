0c44e13fb7b00c254ff7f0e179581255bf46b043
TGEnigma <smartychrisje@hotmail.com>
2017-05-18 17:41:02 +0200

    Reimplemented commandline parser & rewrote current compiler frontend

---
+++ Source/AtlusScriptLib/Common/CommandLineArgumentParser.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlusScriptLib.Common
+{
+    public sealed class CommandLineArgumentParser
+    {
+        private string[] mStrArgs;
+        private int mStrArgIndex;
+        private List<ICommandLineArgument> mArgs;
+        private Dictionary<string, ICommandLineArgument> mArgsByKey;
+
+        public string Description { get; set; }
+
+        public IReadOnlyList<ICommandLineArgument> Arguments
+        {
+            get { return mArgs.AsReadOnly(); }
+        }
+
+        public IReadOnlyDictionary<string, ICommandLineArgument> ArgumentsByKey
+        {
+            get { return new ReadOnlyDictionary<string, ICommandLineArgument>(mArgsByKey); }
+        }
+
+        public CommandLineArgumentParser()
+        {
+            mArgs = new List<ICommandLineArgument>();
+            mArgsByKey = new Dictionary<string, ICommandLineArgument>();
+        }
+
+        public void AddArgument(ICommandLineArgument argument)
+        {
+            mArgs.Add(argument);
+            mArgsByKey[argument.Key] = argument;
+        }
+
+        public void AddArguments(params ICommandLineArgument[] arguments)
+        {
+            foreach (var arg in arguments)
+            {
+                AddArgument(arg);
+            }
+        }
+
+        public void Parse(string[] args)
+        {
+            mStrArgs = args;
+            ParseInternal();
+        }
+
+        public string GetArgumentInfoString()
+        {
+            int columnWidth = 10;
+
+            var stringBuilder = new StringBuilder()
+
+            // info header
+
+            /*
+            .AppendLine()
+            .AppendLine(Description)
+            */
+            .AppendLine()
+            .AppendLine("Possible arguments:")
+            .AppendLine()
+
+            // argument info header row
+            .Append("key".PadRight(columnWidth))
+            .Append("required".PadRight(columnWidth))
+            .Append("default".PadRight(columnWidth))
+            .Append("description".PadRight(columnWidth))
+            .AppendLine();
+
+            foreach (var arg in mArgs)
+            {
+                // argument info row
+                stringBuilder
+                    .Append(arg.Key.PadRight(columnWidth))
+                    .Append((arg.Required ? "yes" : "no").PadRight(columnWidth))
+                    .Append(((arg.DefaultValue ?? "none").ToString()).ToLower().PadRight(columnWidth))
+                    .Append((arg.Description ?? "no description").PadRight(columnWidth))
+                    .AppendLine();
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private void ParseInternal()
+        {
+            while (FindArgumentKey(out string key))
+            {
+                var arg = mArgsByKey[key];
+                var argValues = FindArgumentValues();
+
+                arg.IsValueProvided = true;
+
+                if (arg.TakesParameters)
+                {
+                    if (argValues.Count == 0)
+                        throw new Exception($"Missing parameter for argument {arg.Key}");
+
+                    arg.Value = argValues[0];
+                }
+                else
+                {
+                    arg.Value = true;
+                }
+            }
+
+            VerifyParsedArguments();
+        }
+
+        private void VerifyParsedArguments()
+        {
+            foreach (var arg in Arguments)
+            {
+                if (arg.Required && !arg.IsValueProvided)
+                    throw new Exception($"Missing required argument {arg.Key}");
+
+                if (arg.IsValueProvided && arg.PossibleValues != null && !arg.PossibleValues.Any(x => x == arg.Value))
+                    throw new Exception($"Argument {arg.Key} has an invalid value");
+            }
+        }
+
+        private bool FindArgumentKey(out string key)
+        {
+            while (mStrArgIndex < mStrArgs.Length)
+            {
+                key = mStrArgs[mStrArgIndex++];
+
+                if (key.StartsWith("-"))
+                {
+                    return true;
+                }
+            }
+
+            key = null;
+            return false;
+        }
+
+        private List<string> FindArgumentValues()
+        {
+            var values = new List<string>();
+
+            while (mStrArgIndex < mStrArgs.Length)
+            {
+                string value = mStrArgs[mStrArgIndex++];
+
+                if (value.StartsWith("-"))
+                {
+                    mStrArgIndex--;
+                    break;
+                }
+                else
+                {
+                    values.Add(value);
+                }
+            }
+
+            return values;
+        }
+    }
+}
---
+++ Source/AtlusScriptLib/Common/CommandLineArgumentValueTarget.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace AtlusScriptLib.Common
+{
+    public class CommandLineArgumentValueTarget
+    {
+        public Type TargetType { get; }
+
+        public object TargetInstance { get; }
+
+        public MemberInfo TargetMember { get; }
+
+        public CommandLineArgumentValueTarget(object instance, string memberName)
+        {
+            TargetType = instance.GetType();
+            TargetInstance = instance;
+            TargetMember = GetMemberInfo(memberName);
+        }
+
+        public CommandLineArgumentValueTarget(Type type, string memberName)
+        {
+            TargetType = type;
+            TargetMember = GetMemberInfo(memberName);
+        }
+
+        private MemberInfo GetMemberInfo(string memberName)
+        {
+            var members = TargetType.GetMember(memberName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+
+            if (members.Length == 0)
+            {
+                throw new Exception($"No member with name {memberName} found for type {TargetType.FullName}");
+            }
+            else if (members.Length == 1)
+            {
+                return members[0];
+            }
+            else
+            {
+                throw new Exception($"More than one member with name {memberName} found for type {TargetType.FullName}");
+            }
+        }
+    }
+}
