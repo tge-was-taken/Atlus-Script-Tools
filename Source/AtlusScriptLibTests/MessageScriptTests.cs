db3e7fff4fe8f0588d64e9b323d5712252b65852
TGEnigma <smartychrisje@hotmail.com>
2017-05-29 20:57:51 +0200

    Updated tests for MessageScript::ToBinary

--- Source/AtlusScriptLibTests/MessageScriptTests.cs
+++ Source/AtlusScriptLibTests/MessageScriptTests.cs
@@ -62,12 +62,99 @@ namespace AtlusScriptLib.Tests
         }
 
         [TestMethod()]
-        public void ToBinaryTest()
+        public void ToBinary_ShouldMatchSourceBinary_Version1()
         {
             var binary = MessageScriptBinary.FromFile("TestResources\\Version1.bmd");
             var script = MessageScript.FromBinary(binary);
             var newBinary = script.ToBinary();
-            newBinary.ToFile("TestResources\\Version1_ToFile.bmd");
+
+            Compare(binary, newBinary);
+        }
+
+        [TestMethod()]
+        public void ToBinary_ShouldMatchSourceBinary_Version1BigEndian()
+        {
+            var binary = MessageScriptBinary.FromFile("TestResources\\Version1BigEndian.bmd");
+            var script = MessageScript.FromBinary(binary);
+            var newBinary = script.ToBinary();
+
+            Compare(binary, newBinary);
+        }
+
+        private void Compare(MessageScriptBinary binary, MessageScriptBinary newBinary)
+        {
+            // compare headers
+            Assert.AreEqual(binary.Header.FileType, newBinary.Header.FileType);
+            Assert.AreEqual(binary.Header.IsCompressed, newBinary.Header.IsCompressed);
+            Assert.AreEqual(binary.Header.UserId, newBinary.Header.UserId);
+            Assert.AreEqual(binary.Header.FileSize, newBinary.Header.FileSize);
+            CollectionAssert.AreEqual(binary.Header.Magic, newBinary.Header.Magic);
+            Assert.AreEqual(binary.Header.Field0C, newBinary.Header.Field0C);
+            Assert.AreEqual(binary.Header.RelocationTable.Address, newBinary.Header.RelocationTable.Address);
+            CollectionAssert.AreEqual(binary.Header.RelocationTable.Value, newBinary.Header.RelocationTable.Value);
+            Assert.AreEqual(binary.Header.RelocationTableSize, newBinary.Header.RelocationTableSize);
+            Assert.AreEqual(binary.Header.MessageCount, newBinary.Header.MessageCount);
+            Assert.AreEqual(binary.Header.IsRelocated, newBinary.Header.IsRelocated);
+            Assert.AreEqual(binary.Header.Field1E, newBinary.Header.Field1E);
+
+            for (var index = 0; index < binary.MessageHeaders.Count; index++)
+            {
+                var header = binary.MessageHeaders[index];
+                var newHeader = newBinary.MessageHeaders[index];
+
+                // compare message headers
+                Assert.AreEqual(header.MessageType, newHeader.MessageType);
+                Assert.AreEqual(header.Message.Address, newHeader.Message.Address);
+
+                // compare message data
+                switch (header.MessageType)
+                {
+                    case MessageScriptBinaryMessageType.Dialogue:
+                        {
+                            var dialogue = (MessageScriptBinaryDialogueMessage)header.Message.Value;
+                            var newDialogue = (MessageScriptBinaryDialogueMessage)newHeader.Message.Value;
+
+                            Assert.AreEqual(dialogue.Identifier, newDialogue.Identifier);
+                            Assert.AreEqual(dialogue.LineCount, newDialogue.LineCount);
+                            Assert.AreEqual(dialogue.SpeakerId, newDialogue.SpeakerId);
+                            CollectionAssert.AreEqual(dialogue.LineStartAddresses, newDialogue.LineStartAddresses);
+                            Assert.AreEqual(dialogue.TextBufferSize, newDialogue.TextBufferSize);
+                            CollectionAssert.AreEqual(dialogue.TextBuffer, newDialogue.TextBuffer);
+                        }
+                        break;
+
+                    case MessageScriptBinaryMessageType.Selection:
+                        {
+                            var selection = (MessageScriptBinarySelectionMessage)header.Message.Value;
+                            var newSelection = (MessageScriptBinarySelectionMessage)newHeader.Message.Value;
+
+                            Assert.AreEqual(selection.Identifier, newSelection.Identifier);
+                            Assert.AreEqual(selection.Field18, newSelection.Field18);
+                            Assert.AreEqual(selection.OptionCount, newSelection.OptionCount);
+                            Assert.AreEqual(selection.Field1C, newSelection.Field1C);
+                            Assert.AreEqual(selection.Field1E, newSelection.Field1E);
+                            CollectionAssert.AreEqual(selection.OptionStartAddresses, newSelection.OptionStartAddresses);
+                            Assert.AreEqual(selection.TextBufferSize, newSelection.TextBufferSize);
+                            CollectionAssert.AreEqual(selection.TextBuffer, newSelection.TextBuffer);
+                        }
+                        break;
+                }
+            }
+
+            // compare speaker table header
+            Assert.AreEqual(binary.SpeakerTableHeader.SpeakerNameArray.Address, newBinary.SpeakerTableHeader.SpeakerNameArray.Address);
+            Assert.AreEqual(binary.SpeakerTableHeader.SpeakerCount, newBinary.SpeakerTableHeader.SpeakerCount);
+            Assert.AreEqual(binary.SpeakerTableHeader.Field08, newBinary.SpeakerTableHeader.Field08);
+            Assert.AreEqual(binary.SpeakerTableHeader.Field0C, newBinary.SpeakerTableHeader.Field0C);
+
+            for (int i = 0; i < binary.SpeakerTableHeader.SpeakerNameArray.Value.Length; i++)
+            {
+                var speakername = binary.SpeakerTableHeader.SpeakerNameArray.Value[i];
+                var newSpeakername = newBinary.SpeakerTableHeader.SpeakerNameArray.Value[i];
+
+                Assert.AreEqual(speakername.Address, newSpeakername.Address);
+                CollectionAssert.AreEqual(speakername.Value, newSpeakername.Value);
+            }
         }
     }
 }
