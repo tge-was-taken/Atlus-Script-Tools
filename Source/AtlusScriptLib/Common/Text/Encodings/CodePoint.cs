cbdf0ad8510825d9b24671553fcee510461960fc
TGEnigma <smartychrisje@hotmail.com>
2017-09-02 01:15:19 +0200

    Added Persona5Encoding

---
+++ Source/AtlusScriptLib/Common/Text/Encodings/CodePoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtlusScriptLib.Common.Text.Encodings
+{
+    public struct CodePoint
+    {
+        public byte HighSurrogate;
+        public byte LowSurrogate;
+
+        public CodePoint( byte high, byte low )
+        {
+            HighSurrogate = high;
+            LowSurrogate = low;
+        }
+    }
+}
